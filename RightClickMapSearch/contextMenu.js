06aa57f3711b16f3cf81dc87103c79fd677eb5dd
Brent Komer <brent.komer@hotmail.com>
2013-01-11 22:49:59 -0500

    adding multiple options on context menu

--- RightClickMapSearch/contextMenu.js
+++ RightClickMapSearch/contextMenu.js
@@ -5,6 +5,7 @@
 // A generic onclick callback function.
 function openMapTab(info, tab) {
   var selection = info.selectionText;
+  var id = info.menuItemId; //this will be equivalent to the key in localStorage
   selection = selection.replace("#","");
   console.log("item " + info.menuItemId + " was clicked");
   console.log("info: " + JSON.stringify(info));
@@ -12,8 +13,14 @@ function openMapTab(info, tab) {
   if (navigator.geolocation) {
 	navigator.geolocation.getCurrentPosition(
 	  function(position) {
+	    var dest;
 	    var curLocation = position.coords.latitude+",+"+position.coords.longitude;
-		var URL="http://maps.google.com/?saddr=" +curLocation+"&daddr="+selection;
+		if (id == "contextselection") {
+		  dest = selection;
+		} else {
+		  dest = localStorage[id];
+		}
+		var URL="http://maps.google.com/?saddr=" +curLocation+"&daddr="+dest;
 		chrome.tabs.create({'url': URL}, function(tab) {
 		  // Tab opened.
 		});
@@ -26,9 +33,16 @@ chrome.contextMenus.onClicked.addListener(openMapTab);
 
 // Create one test item for each context type.
 chrome.runtime.onInstalled.addListener(function() {
-	var title = "Open Google Maps";
+	var title = "From Current Location";
 	var context = "selection";
 	var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context});
 	console.log("'" + context + "' item:" + id);
+	localStorage["test1"] = "140+Westmount+Rd.+N,+Waterloo,+ON";
+	localStorage["test2"] = "98+King+St.+N,+Waterloo,+ON";
+	for (var i=0; i<localStorage.length;i++) {
+      title = localStorage.key(i);
+      id = chrome.contextMenus.create({"title": "From "+title, "contexts":[context], "id": title});
+	  console.log("'" + context + "' item:" + id);
+	}
 });
 	
