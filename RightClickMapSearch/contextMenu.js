2ee7a76b9362acc86fc12263ebfb5bf5f7875e40
Ronak Shah <ronak.shah@outlook.com>
2013-01-11 21:39:10 -0500

    Added working right click to search google maps extension

---
+++ RightClickMapSearch/contextMenu.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// A generic onclick callback function.
+function openMapTab(info, tab) {
+  var selection = info.selectionText;
+  selection = selection.replace("#","");
+  var URL="http://maps.google.com/?q=" + selection;
+  chrome.tabs.create({'url': URL}, function(tab) {
+    // Tab opened.
+  });
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+}
+
+chrome.contextMenus.onClicked.addListener(openMapTab);
+
+// Create one test item for each context type.
+chrome.runtime.onInstalled.addListener(function() {
+	var title = "Open Google Maps";
+	var context = "selection";
+	var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context});
+	console.log("'" + context + "' item:" + id);
+});
+	
