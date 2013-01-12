96166fa29556644ecf135e9429350b80db7a7f3d
Shehryar Farooq <sherry.farooq@hotmail.com>
2013-01-12 00:50:50 -0500

    dded functionality for profile manager

--- popup.js
+++ popup.js
@@ -7,15 +7,60 @@
  *
  */
 var KEY_PREFIX = 'easy_map_user.';
-"easy_map_user.home"
 
-var profileReader = {
-	loadProfiles: function(){
-		
+
+function loadProfiles(){
+	var list = document.getElementById("profileList");
+	list.innerHTML = '';
+	for(var key in localStorage){
+		list.innerHTML = list.innerHTML + 
+			"<tr>" +
+				"<td>" +
+					"<img src='deleteProfile.png' id='" + key + "'>" +  "<b>" + key + "</b>" +  "<br>" +
+					localStorage[key] +
+				"</td>" +
+			"</tr>";
 	}
-};
+}
 
-// Run our kitten generation script as soon as the document's DOM is ready.
+function attachEvents(){
+	for(var key in localStorage){
+		var img = document.getElementById(key);
+		img.addEventListener('click', 
+			function(e){
+				removeProfile(e.target.id);
+			});
+	}
+}
+
+function addProfile(){
+	var req_profile_name = document.getElementById("profileName").value;
+	var req_profile_addr = document.getElementById("profileAddr").value;
+	var key = KEY_PREFIX+req_profile_name;
+	var storedKey = localStorage[key];
+	if(req_profile_name == ''){
+		alert("Please enter a profile name");
+	}else if(storedKey != null){
+		alert("This profile name already exists \nPlease enter a new profile name.");
+	}else if(req_profile_addr == ''){
+		alert("Please enter a profile address");
+	}else{
+		localStorage[key] = req_profile_addr;
+		document.getElementById("profileName").value = '';
+		document.getElementById("profileAddr").value = '';
+		loadProfiles();
+		attachEvents();
+	}
+}
+
+function removeProfile(key){
+	localStorage.removeItem(key);
+	var parent = document.getElementById(key).parentNode.parentNode;
+	parent.parentNode.removeChild(parent);
+}
+	
 document.addEventListener('DOMContentLoaded', function () {
-  profileReader.loadProfiles();
+	document.querySelector('button').addEventListener('click', addProfile);
+	loadProfiles();
+	attachEvents();
 });
